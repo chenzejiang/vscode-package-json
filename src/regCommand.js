595e5027033d10d613dff3ce33f8a7818b0ab214
chenzejiang <820289461@qq.com>
2019-07-16 11:03:30 +0800

    增加cnpm 命令

--- src/regCommand.js
+++ src/regCommand.js
@@ -1,30 +1,41 @@
 const vscode = require('vscode');
 module.exports = function(context) {
-    context.subscriptions.push(vscode.commands.registerCommand('coin.focus', () => {
-        let terminal = vscode.window.createTerminal({
-			name: 'vscode-package-json'
-		});
-		terminal.show(true);
-		terminal.sendText('ls');
-    }));
-    /**
-     * 删除 node_modules 文件夹
-     */
-    context.subscriptions.push(vscode.commands.registerCommand('package-json.rm', () => {
-        let terminal = vscode.window.createTerminal({
-			name: 'vscode-package-json'
-		});
-		terminal.show(true);
-		terminal.sendText('rm -rf node_modules');
-    }));
-    /**
-     * 安装依赖
-     */
-    context.subscriptions.push(vscode.commands.registerCommand('package-json.npm.install', () => {
-        let terminal = vscode.window.createTerminal({
-			name: 'vscode-package-json'
-		});
-		terminal.show(true);
-		terminal.sendText('npm install --registry=https://registry.npm.taobao.org');
-    }));
+  context.subscriptions.push(vscode.commands.registerCommand('package-json.ls', (cmd) => {
+    let terminal = vscode.window.createTerminal({
+      name: 'vscode-package-json'
+    });
+    terminal.show(true);
+    terminal.sendText(cmd);
+  }));
+  /**
+   * 删除 node_modules 文件夹
+   */
+  context.subscriptions.push(vscode.commands.registerCommand('package-json.rm', () => {
+    let terminal = vscode.window.createTerminal({
+      name: 'vscode-package-json'
+    });
+    terminal.show(true);
+    terminal.sendText('rm -rf node_modules');
+  }));
+  /**
+   * 安装依赖
+   */
+  context.subscriptions.push(vscode.commands.registerCommand('package-json.npm.install', () => {
+    let terminal = vscode.window.createTerminal({
+      name: 'vscode-package-json'
+    });
+    terminal.show(true);
+    terminal.sendText('npm install');
+  }));
+  
+  /**
+   * 安装cnpm install
+   */
+  context.subscriptions.push(vscode.commands.registerCommand('package-json.cnpm.install', () => {
+    let terminal = vscode.window.createTerminal({
+      name: 'vscode-package-json'
+    });
+    terminal.show(true);
+    terminal.sendText('cnpm install');
+  }));
 };
