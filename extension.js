68e934c03452d83a7daf0f0e5d5be5e8cf178a21
chenzejiang <820289461@qq.com>
2019-07-16 11:03:55 +0800

    删除注释

--- extension.js
+++ extension.js
@@ -2,18 +2,15 @@
  * 插件被激活时触发，所有代码总入口
  * @param {*} context 插件上下文
  */
-// @ts-ignore
-// const package1 = require("./package.json");
 const App = require('./src/app');
 exports.activate = function(context) {
     require('./src/regCommand')(context);
     new App(context);
-    // console.log(package1);
 };
 
 /**
  * 插件被释放时触发
  */
 exports.deactivate = function() {
-    console.log('您的扩展已被释放！')
+    console.log('扩展已被释放！')
 };
